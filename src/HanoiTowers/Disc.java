4779730beb645258b3589a26876ccd9613207982
Karen Vardanyan <karenvardanyan@workfront.com>
2016-04-11 13:49:25 +0400

    initial

---
+++ src/HanoiTowers/Disc.java
@@ -0,0 +1,15 @@
+package HanoiTowers;
+
+import java.awt.*;
+import figure.Rectangle;
+
+public class Disc extends Rectangle {
+
+    private Tower myTower;
+
+    public Disc(int x, int y, int width, int height, Color color, HanoCanvas hanoCanvas){
+        super(x, y, width, height, hanoCanvas, color);
+    }
+
+
+}
---
+++ src/HanoiTowers/HanoCanvas.java
@@ -0,0 +1,19 @@
+package HanoiTowers;
+
+
+
+import java.awt.*;
+import figure.FigureCanvas;
+
+public class HanoCanvas extends FigureCanvas {
+
+    public HanoCanvas() {
+    }
+
+    public void load(int n) {
+    }
+
+    public void paint(Graphics g) {
+    }
+
+}
