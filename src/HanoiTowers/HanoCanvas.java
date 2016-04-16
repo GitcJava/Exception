626b39fd3124d38566c27a10745b29a882dce482
sraphunanyan <hunsrap747@gmail.com>
2016-04-16 22:01:36 +0400

    I create a select method

--- src/HanoiTowers/HanoCanvas.java
+++ src/HanoiTowers/HanoCanvas.java
@@ -7,7 +7,31 @@ import figure.FigureCanvas;
 
 public class HanoCanvas extends FigureCanvas {
 
+    public Tower first = new Tower(30, 170, this);
+    public Tower middle = new Tower(first.getX() + Tower.horWidth + 30, 170, this);
+    public Tower end = new Tower(middle.getX() + Tower.horWidth + 30, 170, this);
+
+    private Tower selected;
+    boolean isSelected;
+
+
     public HanoCanvas() {
+    }
+
+    private void select(int x, int y) {
+        isSelected = false;
+        if (first.isBelong(x, y)) {
+            selected = first;
+            isSelected = true;
+        } else if (middle.isBelong(x, y)) {
+            selected = middle;
+            isSelected = true;
+        } else if (end.isBelong(x, y)) {
+            selected = end;
+            isSelected = true;
+        } else {
+            isSelected = false;
+        }
     }
 
     public void load(int n) {
