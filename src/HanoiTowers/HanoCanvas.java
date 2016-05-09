6ca1718aa350921ba2c753813214bcb83976b1a9
Karen Vardanyan <karenvardanyan@workfront.com>
2016-05-09 13:58:01 +0400

    Merge remote-tracking branch 'origin/develop'
    # Conflicts:
    #	src/HanoiTowers/HanoCanvas.java   Is corrected

--- src/HanoiTowers/HanoCanvas.java
+++ src/HanoiTowers/HanoCanvas.java
@@ -3,41 +3,136 @@ package HanoiTowers;
 
 
 import java.awt.*;
+
 import figure.FigureCanvas;
 
-public class HanoCanvas extends FigureCanvas {
+public class HanoCanvas extends FigureCanvas implements Runnable {
 
-    public Tower first = new Tower(30, 170, this);
+    public Tower first = new Tower(45, 170, this);
     public Tower middle = new Tower(first.getX() + Tower.horWidth + 30, 170, this);
     public Tower end = new Tower(middle.getX() + Tower.horWidth + 30, 170, this);
 
-    private Tower selected;
-    boolean isSelected;
+    private boolean isStarted = false;
+    private int discsNum;
+    Thread t;
+    Disc curr;
 
 
     public HanoCanvas() {
+        setBackground(Color.CYAN);
     }
 
-    private void select(int x, int y) {
-        isSelected = false;
-        if (first.isBelong(x, y)) {
-            selected = first;
-            isSelected = true;
-        } else if (middle.isBelong(x, y)) {
-            selected = middle;
-            isSelected = true;
-        } else if (end.isBelong(x, y)) {
-            selected = end;
-            isSelected = true;
+    public void load(int discsNum) {
+        Clean();
+        first.addDiscs(discsNum);
+        repaint();
+    }
+
+
+    public void start() {
+        t = new Thread(this);
+        t.start();
+        isStarted = true;
+    }
+
+
+
+    private void select(){}
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        first.draw(g);
+        middle.draw(g);
+        end.draw(g);
+
+    }
+    private void Clean() {
+        isStarted = false;
+        first.getDiscList().clear();
+        middle.getDiscList().clear();
+        end.getDiscList().clear();
+    }
+
+    @Override
+    public void run() {
+        moveDiscs(first.getDiscList().size(), first, middle, end);
+    }
+
+
+
+    private void moveDiscs(int number, Tower first, Tower middle, Tower end) {
+        if (number == 1) {
+            move(first, middle, end);
         } else {
-            isSelected = false;
+            moveDiscs(number - 1, first, end, middle);
+            move(first, middle, end);
+            moveDiscs(number - 1, middle, first, end);
         }
     }
 
-    public void load(int n) {
+    private void move(Tower first, Tower middle, Tower end) {
+        up(first, middle, end);
+        update(getGraphics());
     }
 
-    public void paint(Graphics g) {
+
+    public void up(Tower first, Tower middle, Tower end) {
+        curr = first.getDiscList().get(first.getDiscList().size() - 1);
+        end.getDiscList().add(first.getDiscList().get(first.getDiscList().size() - 1));
+        first.getDiscList().remove(first.getDiscList().get(first.getDiscList().size() - 1));
+        while (curr.getY() > first.getY() - curr.getHeight()) {
+            curr.move(0, -1);
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (first.getX() > end.getX()) {
+            left(first, middle, end);
+        } else {
+            right(first, middle, end);
+        }
     }
 
+
+    public void right(Tower first, Tower middle, Tower end) {
+        while (curr.getX() < end.getX() + ((end.getWidth() - curr.getWidth()) / 2)) {
+            curr.move(1, 0);
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        down(first, middle, end);
+    }
+
+
+    public void down(Tower first, Tower middle, Tower end) {
+        while (curr.getY() < end.getY() + end.getHeight() - Tower.horHeight - end.getDiscList().size() * (8+1)) {
+            curr.move(0, 1);
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void left(Tower first, Tower middle, Tower end) {
+        while (curr.getX() > end.getX() + ((end.getWidth() - curr.getWidth()) / 2)) {
+            curr.move(-1, 0);
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        down(first, middle, end);
+    }
 }
